abd4d10a20f3c2d49db146b2d057698fc356c771
ytrofnom <ysabelmonfort@gmail.com>
2017-12-29 12:11:14 +0100

    Limits a user's post to 140 characters.

--- js/main.js
+++ js/main.js
@@ -5,8 +5,20 @@ $(document).ready(() => {
     $('.nav-menu').hide();
   });
   $('.btn').on('mouseenter', event => {
-    $(event.currentTarget).addClass('.btn-hover');
+    $(event.currentTarget).addClass('btn-hover');
   }).on('mouseleave', event => {
-    $(event.currentTarget).removeClass(.'btn-hover');
-  })
-}); 
+    $(event.currentTarget).removeClass('btn-hover');
+  });
+  $('.postText').on('keyup', event => {
+    post = $(event.currentTarget).val();
+    remaining = (140 - post.length);
+    if(remaining <= 0) {
+      $('.wordcount').addClass('red');
+    } else {
+      $('.wordcount').removeClass('red');
+    }
+    $('.characters').html(remaining);
+  });
+  $('.postText').on('focus', () => {
+  });
+});
