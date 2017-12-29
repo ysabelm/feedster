cadf59db1e86dbed888a34bdbed1fa84f4ce5f6f
ytrofnom <ysabelmonfort@gmail.com>
2017-12-29 11:36:02 +0100

    Adds hover functionality to the +1 button elements.

--- js/main.js
+++ js/main.js
@@ -3,5 +3,10 @@ $(document).ready(() => {
     $('.nav-menu').show();
   }).on('mouseleave', () => {
     $('.nav-menu').hide();
+  });
+  $('.btn').on('mouseenter', event => {
+    $(event.currentTarget).addClass('.btn-hover');
+  }).on('mouseleave', event => {
+    $(event.currentTarget).removeClass(.'btn-hover');
   })
 }); 
